a85cc0f35f2482a4b6389874e4ef794a4a4a97cf
addi00000 <addison@othius.com>
2022-06-05 14:54:35 -0400

    Fixed: AttributeError: '_io.TextIOWrapper' object has no attribute 'built'

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-PYTHON -c "exec('''with open('main.py', 'r', encoding='utf-8') as f: content = f.read().replace('WEBHOOK_URL', str(input('Webhook URL: ')))\nwith open('built.py', 'w', encoding='utf-8') as f: f.built(content)''')"
+PYTHON -c "exec('''with open('main.py', 'r', encoding='utf-8') as f: content = f.read().replace('WEBHOOK_URL', str(input('Webhook URL: ')))\nwith open('built.py', 'w', encoding='utf-8') as f: f.write(content)''')"
 ECHO INTALLING REQUIREMENTS && pip install --upgrade -r requirements.txt
 ECHO INSTALLING UPX && curl -L -O https://github.com/upx/upx/releases/download/v3.96/upx-3.96-win64.zip && tar -xf upx-3.96-win64.zip 
 ECHO COMPILING TO EXE && python -m PyInstaller --onefile --noconsole --upx-dir ./upx-3.96-win64 -i NONE --distpath ./ .\built.py 
