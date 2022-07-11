4373c1a4a5275de4c6885ac3fd08363d9bbc7a07
addi00000 <addison@othius.com>
2022-07-11 14:51:33 -0400

    Fixed nextcord issue

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 @MODE 101,50
 
+ECHO REMOVING INTERFERENCES && PY -3.10 -m pip uninstall -r interferences.txt
 ECHO INSTALLING REQUIREMENTS && PY -3.10 -m pip install --upgrade -r requirements.txt
 CLS && PY -3.10 builder.py
 PAUSE && COLOR 07 && EXIT
