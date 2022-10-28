a84e4e64556a5e36b4a3bb3adf257637b7442236
addi00000 <addison@othius.com>
2022-10-27 22:53:24 -0400

    New builder; working

--- build.bat
+++ build.bat
@@ -1,7 +1,12 @@
-@ECHO OFF
-@MODE 101,50
+@echo off
 
-ECHO REMOVING INTERFERENCES && PY -3.10 -m pip uninstall -r interferences.txt
-ECHO INSTALLING REQUIREMENTS && PY -3.10 -m pip install --force-reinstall -r requirements.txt
-CLS && CD BUILD && PY -3.10 main.py
-PAUSE && COLOR 07 && EXIT
+py -3.10 -m pip uninstall -r interferences.txt
+py -3.10 -m pip install --force-reinstall -r requirements.txt
+
+cls
+cd builder
+
+if exist build rmdir /s /q build
+py -3.10 main.py
+
+pause
