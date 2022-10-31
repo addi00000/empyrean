d5105bf0a369d6c3d16134b955c3b3834de9d21d
addi00000 <addison@othius.com>
2022-10-30 22:56:06 -0400

    fix builder issues

--- build.bat
+++ build.bat
@@ -4,9 +4,8 @@ py -3.10 -m pip uninstall -r interferences.txt
 py -3.10 -m pip install --force-reinstall -r requirements.txt
 
 cls
-cd builder
 
 if exist build rmdir /s /q build
-py -3.10 main.py
+py -3.10 builder.py
 
 pause
