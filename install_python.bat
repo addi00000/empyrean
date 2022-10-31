34dca804e9de2f057ee4aafeef2cf3a1f43602a8
addi00000 <addison@othius.com>
2022-10-30 22:34:24 -0400

    Better install python

--- install_python.bat
+++ install_python.bat
@@ -1,7 +1,13 @@
-@ECHO OFF
+@echo off
 
-ECHO DOWNLOADING PYTHON INSTALLER && curl -L -O https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe
-ECHO RUNNING INSTALLER && python-3.10.5-amd64.exe /quiet PrependPath=1 Include_test=0
-ECHO REMOVING INSTALLER EXECUTABLE && del python-3.10.5-amd64.exe
-COLOR 02 && ECHO DONE && ECHO PYTHON (3.10.5) INSTALLED && START .
-PAUSE && COLOR 07 && EXIT
+for /f "tokens=1,2 delims= " %%a in ('powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/ -UseBasicParsing | Select-String -Pattern '3.10.[0-9]{1,2}' -AllMatches | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value | Sort-Object -Descending -Unique | Select-Object -First 1"') do (
+    set "PYTHON_VERSION=%%a%%b"
+)
+set "PYTHON_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe"
+set "PYTHON_EXE=python-installer.exe"
+
+curl -L -o %PYTHON_EXE% %PYTHON_URL%
+
+start /wait %PYTHON_EXE% /quiet /passive InstallAllUsers=0 PrependPath=1 Include_test=0 Include_pip=1 Include_doc=0
+
+del %PYTHON_EXE%
