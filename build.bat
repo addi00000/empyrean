725ba0ef13eb8130c1326e132cafc6250ed400cf
K.Dot <110929758+KDot227@users.noreply.github.com>
2022-11-23 20:53:22 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,14 @@
 @echo off
 
+@echo off
+
+python --version 2>&1 | findstr " 3.11" >nul
+if %errorlevel% == 0 (
+    echo YOU HAVE PYTHON 3.11 INSTALLED. NOTE THIS CAN LEAD TO ERRORS. PLEASE INSTALL PYTHON VERSION 3.10.8
+    timeout -T 5 >nul
+    exit
+)
+
 py -3.10 -m pip uninstall -r interferences.txt
 py -3.10 -m pip install --force-reinstall -r requirements.txt
 
