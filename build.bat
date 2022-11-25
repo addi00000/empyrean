7c13cb77a6e2b60fdbac9810d24ccdee77255161
addi00000 <81730200+addi00000@users.noreply.github.com>
2022-11-24 19:29:54 -0500

    Remove duplicate line

--- build.bat
+++ build.bat
@@ -1,7 +1,5 @@
 @echo off
 
-@echo off
-
 python --version 2>&1 | findstr " 3.11" >nul
 if %errorlevel% == 0 (
     echo python 3.11.x and up are not supported by empyrean. Please downgrade to python 3.10.x.
