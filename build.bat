d4e2550803a9fd936386e9122043191f1a180e82
addi00000 <addison@othius.com>
2022-11-24 20:01:04 -0500

    Git check

--- build.bat
+++ build.bat
@@ -3,7 +3,15 @@
 python --version 2>&1 | findstr " 3.11" >nul
 if %errorlevel% == 0 (
     echo python 3.11.x and up are not supported by empyrean. Please downgrade to python 3.10.x.
-    timeout -T 3 >nul
+
+    pause
+    exit
+)
+
+git --version 2>&1 | findstr "git version" >nul
+if not %errorlevel% == 0 (
+    echo git is not installed. Please install git from https://git-scm.com/download/win
+
     pause
     exit
 )
