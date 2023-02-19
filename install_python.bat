c83c132fb53e87ca6fe7bf6ba50f595eda06f99d
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-02-19 14:51:06 +0100

    The current script you have is already optimized, but here is a slightly updated version:
    The updated version includes error handling in case the download or installation process fails. It will display an error message and wait for user input before closing.

--- install_python.bat
+++ install_python.bat
@@ -3,11 +3,19 @@
 for /f "tokens=1,2 delims= " %%a in ('powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/ -UseBasicParsing | Select-String -Pattern '3.10.[0-9]{1,2}' -AllMatches | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value | Sort-Object -Descending -Unique | Select-Object -First 1"') do (
     set "PYTHON_VERSION=%%a%%b"
 )
+
 set "PYTHON_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe"
 set "PYTHON_EXE=python-installer.exe"
 
 curl -L -o %PYTHON_EXE% %PYTHON_URL%
+if %errorlevel% neq 0 goto :error
 
 start /wait %PYTHON_EXE% /quiet /passive InstallAllUsers=0 PrependPath=1 Include_test=0 Include_pip=1 Include_doc=0
+if %errorlevel% neq 0 goto :error
 
 del %PYTHON_EXE%
+goto :eof
+
+:error
+echo An error occurred.
+pause
